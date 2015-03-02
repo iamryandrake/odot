0739862cc11ee96b204e121a59d11b57d9023a01
Ryan Drake <ryan@ryandrake.com>
2015-03-02 19:46:22 +1000

    Edit routes; clean up comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   resources :todo_lists
   root 'todo_lists#index'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
