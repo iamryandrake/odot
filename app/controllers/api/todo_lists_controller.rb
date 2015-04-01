a6bab85238ba8ca55d2cadb03aef5021620c04e8
Ryan Drake <ryan@ryandrake.com>
2015-04-01 13:02:25 +1000

    Add create method to api

--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -1,4 +1,6 @@
 class Api::TodoListsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def index
     render json: TodoList.all
   end
@@ -6,5 +8,19 @@ class Api::TodoListsController < ApplicationController
   def show
     list = TodoList.find(params[:id])
     render json: list
+  end
+
+  def create
+    list = TodoList.new(list_params)
+    if list.save
+      head 200
+    else
+      head 500
+    end
+  end
+
+  private
+  def list_params
+    params.require('todo_list').permit(:title, :description)
   end
 end
