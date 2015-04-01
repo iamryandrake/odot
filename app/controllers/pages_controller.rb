2b6674d9aab8ce6c8661b9469ef92bae3ea3fdbf
Ryan Drake <ryan@ryandrake.com>
2015-04-01 10:03:13 +1000

    Modified pages controller to redirect to to do lists if logged in else home; add some new stylesheets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
+
   def home
-    render layout: 'home'
+    if user_signed_in?
+      redirect_to todo_lists_path and return
+    else
+      render layout: 'home'
+    end
   end
 end
