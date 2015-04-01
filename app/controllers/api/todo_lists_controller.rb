48e4dfe08eda7a5f62669b22e676e545ff22c700
Ryan Drake <ryan@ryandrake.com>
2015-04-01 11:55:03 +1000

    Add show method to api

--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -2,4 +2,9 @@ class Api::TodoListsController < ApplicationController
   def index
     render json: TodoList.all
   end
+
+  def show
+    list = TodoList.find(params[:id])
+    render json: list
+  end
 end
