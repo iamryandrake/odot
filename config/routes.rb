6f44966191e1151a0009afd5c0fd9365b4d00c14
Ryan Drake <ryan@ryandrake.com>
2015-04-01 18:16:38 +1000

    Add routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
 
   get 'pages/home'
 
+  get "/login" => "user_sessions#new", as: :login
+  delete "/logout" => "user_sessions#destroy", as: :logout
+
+  resources :users
+  resources :user_sessions, only: [:new, :create]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+
   # get '/login' => 'user_sessions#new', as: :login
   # delete '/logout' => 'user_sessions#destroy', as: :logout
 
